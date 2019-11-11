349688806468e109cfb607132d542aa47892c267
HirushaR <hirusharandunu11@gmail.com>
2019-11-11 21:27:58 +0530

    set the window to update more than 10 frame per second

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NFS_payback_automation
+
+first i did is i get screen server and grab the location of payback
--- payback-1.py
+++ payback-1.py
@@ -1,12 +1,17 @@
 import numpy as np
 from PIL import ImageGrab
 import cv2
+import time
 
+last_time = time.time()
 while(True):
     printscreen_pil = ImageGrab.grab(bbox=(0, 40, 800, 1000))
-    printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8')\
-    .reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
-    cv2.imshow('window',printscreen_numpy)
+    #printscreen_numpy = np.array(printscreen_pil.getdata(),dtype='uint8')#\
+    # .reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
+    #
+    print('Loop took {} second'.format(time.time()-last_time))
+    last_time = time.time()
+    cv2.imshow('window',np.array(printscreen_pil))
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
