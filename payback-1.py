95d151b3a7c8d46ff72aaa728e09c4e222e974a9
HirushaR <hirusharandunu11@gmail.com>
2019-11-11 21:49:45 +0530

    get little edge from canny

--- payback-1.py
+++ payback-1.py
@@ -14,10 +14,12 @@ def process_img(original_image):
 last_time = time.time()
 while(True):
     screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 1000)))
+    new_screen = process_img(screen)
 
     print('Loop took {} second'.format(time.time()-last_time))
     last_time = time.time()
-    cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    cv2.imshow('window', new_screen)
+    #cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
