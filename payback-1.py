0a8d2895b49bf8a63adebb630c461c8640813f8b
HirushaR <hirusharandunu11@gmail.com>
2019-11-12 09:39:06 +0530

    upadte the code

--- payback-1.py
+++ payback-1.py
@@ -4,15 +4,6 @@ import cv2
 import time
 from key_direct import PressKey, W , A, S, D, ReleaseKey
 
-for i in list(range(4))[::-1]:
-    print(i+1)
-    time.sleep(1)
-print('down')
-PressKey(W)
-time.sleep(3)
-print('up')
-ReleaseKey(W)
-
 
 
 
@@ -21,16 +12,25 @@ def process_img(original_image):
     processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
     processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
     return processed_img
-#
-# last_time = time.time()
-# while(True):
-#     screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 1000)))
-#     new_screen = process_img(screen)
-#
-#     print('Loop took {} second'.format(time.time()-last_time))
-#     last_time = time.time()
-#     cv2.imshow('window', new_screen)
-#     #cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-#     if cv2.waitKey(25) & 0xFF == ord('q'):
-#         cv2.destroyAllWindows()
-#         break
+
+for i in list(range(4))[::-1]:
+    print(i+1)
+    time.sleep(1)
+
+last_time = time.time()
+while(True):
+    screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 1000)))
+    new_screen = process_img(screen)
+    print('down')
+    PressKey(W)
+    time.sleep(3)
+    print('up')
+    ReleaseKey(W)
+    print('Loop took {} second'.format(time.time()-last_time))
+    last_time = time.time()
+    #time.sleep(3) with delay our new screen in 3 sec
+    cv2.imshow('window', new_screen)
+    #cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
