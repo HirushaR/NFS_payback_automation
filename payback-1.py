113e8bf5b59beba6d9b1865c1e528c995439765a
HirushaR <hirusharandunu11@gmail.com>
2019-11-12 10:08:55 +0530

    change the thresh holds

--- payback-1.py
+++ payback-1.py
@@ -4,7 +4,7 @@ import cv2
 import time
 from key_direct import PressKey, W , A, S, D, ReleaseKey
 
-def roi(img , vertices):
+def roi(img, vertices):
     # shape convert to zeros
     mask = np.zeros_like(img)
     cv2.fillPoly(mask, vertices, 255)
@@ -15,10 +15,10 @@ def roi(img , vertices):
 def process_img(original_image):
     # covert original image to gray
     processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-    processed_img = cv2.Canny(processed_img, threshold1=300, threshold2=200)
+    processed_img = cv2.Canny(processed_img, threshold1=250, threshold2=150)
     # shape of the road we want to identify i put a image later
-    vertices = np.array([10, 500], [10, 300], [300, 200], [500, 200], [800, 300], [800,500] )
-    processed_img = roi(processed_img, vertices)
+    vertices = np.array([[10, 500], [10, 300], [300, 200], [500, 200], [800, 300], [800,500]])
+    processed_img = roi(processed_img, [vertices])
     return processed_img
 
 for i in list(range(4))[::-1]:
