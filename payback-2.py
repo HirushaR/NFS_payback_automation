6962b7c659823052eae441b0bc5901704b95aa42
HirushaR <hirusharandunu11@gmail.com>
2019-11-14 21:07:11 +0530

    convert to the gray

--- payback-2.py
+++ payback-2.py
@@ -1,12 +1,19 @@
 import numpy as np
 from PIL import ImageGrab
 import cv2
+import time
 
+last_time = time.time()
 while(True):
     screen =  np.array(ImageGrab.grab(bbox=(0, 40, 800, 600)))
     # printscreen_numpy =   np.array(printscreen_pil.getdata(),dtype='uint8')\
     # .reshape((printscreen_pil.size[1],printscreen_pil.size[0],3))
-    cv2.imshow('window', screen)
+
+    print('Loop took {} second'.format(time.time()- last_time))
+    last_time = time.time()
+    gray_screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+    #cv2.imshow('window', screen)
+    cv2.imshow('window2', gray_screen)
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
