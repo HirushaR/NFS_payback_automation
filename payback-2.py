62aa7addae781b884cabc039f1e7e5bf348f535b
HirushaR <hirusharandunu11@gmail.com>
2019-11-14 21:09:37 +0530

    blur the image

--- payback-2.py
+++ payback-2.py
@@ -11,9 +11,13 @@ while(True):
 
     print('Loop took {} second'.format(time.time()- last_time))
     last_time = time.time()
+
+    # convert to gray
     gray_screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+    # blur the gray image
+    blur_screen = cv2.GaussianBlur(gray_screen, (5, 5), 0)
     #cv2.imshow('window', screen)
-    cv2.imshow('window2', gray_screen)
+    cv2.imshow('window2', blur_screen)
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
