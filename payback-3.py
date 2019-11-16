64c36b840ae926244e21593bec2a87b03f9d29bc
HirushaR <hirusharandunu11@gmail.com>
2019-11-16 13:27:38 +0530

    pause the data

--- payback-3.py
+++ payback-3.py
@@ -1,14 +1,18 @@
 import numpy as np
+from grabscreen import grab_screen
 import cv2
 import time
+from get_keys import key_check
 import os
 
-from get_keys import key_check
-from grabscreen import grab_screen
 
 def keys_to_output(keys):
-    # [A,W,D]
+    '''
+    Convert keys to a ...multi-hot... array
+    [A,W,D] boolean values.
+    '''
     output = [0, 0, 0]
+
     if 'A' in keys:
         output[0] = 1
     elif 'D' in keys:
@@ -17,33 +21,50 @@ def keys_to_output(keys):
         output[1] = 1
     return output
 
-file_name = 'traning_data1.npy'
+
+file_name = 'training_data.npy'
 
 if os.path.isfile(file_name):
-    print('File exist, loading previous data!')
+    print('File exists, loading previous data!')
     training_data = list(np.load(file_name))
 else:
-    print('File does not exist, starting data')
+    print('File does not exist, starting fresh!')
     training_data = []
+
 
 def main():
     for i in list(range(4))[::-1]:
-        print(i+1)
+        print(i + 1)
         time.sleep(1)
 
-    last_time = time.time()
-    while True:
-        screen = grab_screen(region=(0, 40, 800, 640))
-        screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
-        screen = cv2.resize(screen, (80, 60))
-        keys = key_check()
-        output = keys_to_output(keys)
-        training_data.append([screen, output])
-        print('Frame took {} seconds'.format(time.time() - last_time))
-        last_time = time.time()
+    paused = False
+    while (True):
 
-        if len(training_data) % 500 == 0:
-            print(len(training_data))
-            np.save(file_name, training_data)
+        if not paused:
+            # 800x600 windowed mode
+            screen = grab_screen(region=(0, 40, 800, 640))
+            last_time = time.time()
+            screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+            screen = cv2.resize(screen, (160, 120))
+            # resize to something a bit more acceptable for a CNN
+            keys = key_check()
+            output = keys_to_output(keys)
+            training_data.append([screen, output])
+
+            if len(training_data) % 1000 == 0:
+                print(len(training_data))
+                np.save(file_name, training_data)
+
+        keys = key_check()
+        if 'T' in keys:
+            if paused:
+                paused = False
+                print('unpaused!')
+                time.sleep(1)
+            else:
+                print('Pausing!')
+                paused = True
+                time.sleep(1)
+
 
 main()
